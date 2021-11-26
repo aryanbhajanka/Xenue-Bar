d3cd6d01cb4642d0da2cfc4717194cc122399a47
Aryan Bhajanka <85822773+aryanbhajanka@users.noreply.github.com>
2021-11-26 17:50:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -15,65 +15,73 @@ import psutil
 import os
 import win32api, win32con, win32gui, win32process
 from win32con import VK_MEDIA_PLAY_PAUSE, KEYEVENTF_EXTENDEDKEY
+from spotify import Helper
+import threading
+
 
 def spotify():
+    #def func1():
     sp = tk.Toplevel()
-    sp.geometry("930x35+480+0")
+    sp.geometry("915x35+480+0")
     sp.attributes('-alpha',0.9)
     sp.configure(background='#182427')
     canvas = tk.Canvas(sp, height=150, width=400)
     windll.shcore.SetProcessDpiAwareness(1)
     sp.overrideredirect(True)
     sp.attributes('-topmost',True)
+    x = 1
     
-    class Helper():
-        play_pause = 0xB3
-        next_track = 0xB0
-        previous_track = 0xB1
-            
-        def __init__(self):
-            self.hwnd = 0
-
-        def winEnumHandler(self, hwnd, ctx ):
-            process_name = psutil.Process(win32process.GetWindowThreadProcessId(hwnd)[-1]).name()
-            window_name = win32gui.GetWindowText(hwnd)
-            if win32gui.IsWindowVisible(hwnd) and process_name == "Spotify.exe" and window_name != "":
-                self.hwnd = hwnd
-
-        def sendKey(self, key):
-            win32api.keybd_event(key, 0, 0, 0)
-            win32api.keybd_event(key, 0, win32con.KEYEVENTF_KEYUP, 0)
-
-        def getInfo(self):
-            win32gui.EnumWindows(self.winEnumHandler, None)
-            return win32gui.GetWindowText(self.hwnd)
-
     def quitapp():
         sp.destroy()
 
-    hp = Helper()
-    info = hp.getInfo()
-    long_song = Label(sp, text=info[0:45]+'...', bg='#182427', fg='white', font = ('calibri', 10, 'bold'))
-    song = Label(sp, text=info, bg='#182427', fg='white', font = ('calibri', 10, 'bold'))
-    idle = Label(sp, text='Your Spotify Player Is Idle', bg='#182427', fg='white', font = ('calibri', 10, 'bold'))
-    Button(sp,text="Close",command=quitapp, bg='#182427', fg='white',borderwidth=0, highlightthickness=0).place(relx=0.05, rely=0.495, anchor='e')
-    Button(sp, text='||◁', bg='#182427', fg='white',borderwidth=0, highlightthickness=0).place(relx=0.89, rely=0.495, anchor='e')
-    Button(sp, text='| |', bg='#182427', fg='white',borderwidth=0, highlightthickness=0).place(relx=0.926, rely=0.495, anchor='e')
-    Button(sp, text='▷||', bg='#182427', fg='white',borderwidth=0, highlightthickness=0).place(relx=0.97, rely=0.495, anchor='e')
+    def prev():
+        pyautogui.press('prevtrack')
 
-    if(info == 'Spotify Premium'):
-        idle.place(anchor='e', relx=0.6, rely=0.49)
-    elif(info == ''):
-        idle.place(anchor='e', relx=0.6, rely=0.49)
-    else: 
-        if (len(info)>45):
-            long_song.place(anchor='e', relx=0.6, rely=0.49)
-        else:
-           song.place(anchor='e', relx=0.6, rely=0.49)
+    def pause():
+        pyautogui.press('playpause')
 
-   
-                   
+    def next():
+        pyautogui.press('nexttrack')
 
+    def mute():
+        pyautogui.press('volumemute')
+
+    song = Label(sp, text='', bg='#182427', fg='white', font = ('calibri', 10, 'bold'), border=0, highlightthickness=0,justify=CENTER, relief='sunken')    
+    song.place(anchor='e', relx=0.58, rely=0.49)
+    Button(sp,text="Close",command=quitapp, bg='#182427', fg='white',borderwidth=0, highlightthickness=0).place(relx=0.055, rely=0.495, anchor='e')
+    Button(sp, text='🔇', bg='#182427', fg='white',borderwidth=0, highlightthickness=0, command=mute, font=('', 13)).place(relx=0.985, rely=0.495, anchor='e')
+    Button(sp, text='⏮️', bg='#182427', fg='white',borderwidth=0, highlightthickness=0, command=prev, font=('', 10)).place(relx=0.859, rely=0.495, anchor='e')
+    Button(sp, text='⏯️', bg='#182427', fg='white',borderwidth=0, highlightthickness=0, command=pause, font=('', 10)).place(relx=0.897, rely=0.495, anchor='e')
+    Button(sp, text='⏭️', bg='#182427', fg='white',borderwidth=0, highlightthickness=0, command=next, font=('', 10)).place(relx=0.935, rely=0.495, anchor='e')
+
+
+    def update():
+        hp = Helper()
+        info = hp.getInfo()
+        if(info == 'Spotify Premium'):
+            song.place_configure(relx=0.56)
+            song.config(text='Your Spotify Player Is Idle')
+        elif(info == ''):
+            song.place_configure(relx=0.56)
+            song.config(text='Your Spotify Player Is Idle') 
+        else: 
+            if (len(info)>45):
+                song.place_configure(relx=0.65)
+                song.config(text=info[0:45]+'...')
+            elif(len(info)>20):
+                song.place_configure(relx=0.62)
+                song.config(text=info)
+            else:
+                song.config(text=info)
+#20
+    def func2():
+        while 0==0:
+            update()
+    
+    t = threading.Thread(target=func2)
+    t.start()
+    
+          
 def calendar():
     Window = tk.Toplevel()
     Window.geometry("400x300+1500+35")
@@ -97,7 +105,7 @@ def calendar():
 
 def power():
     app_power = tk.Toplevel()
-    app_power.geometry("250x250+50+35")
+    app_power.geometry("250x170+50+35")
     app_power.attributes('-alpha',0.95)
     app_power.configure(background='#182427')
     windll.shcore.SetProcessDpiAwareness(1)
@@ -110,10 +118,10 @@ def power():
         os.system("shutdown /s /t 1")
 
     def restart():
-        os.system("restart /s /t 1")
+        os.system("shutdown /r /t 1")
 
-    def sleep():
-        os.system("sleep /s /t 1")
+    def lock():
+        os.system("Rundll32.exe user32.dll,LockWorkStation")
  
     def quitapp():
         menu.destroy()
@@ -121,7 +129,7 @@ def power():
     Button(app_power,text="x",command=quit, height=1, width=3, bg='red', borderwidth=0, highlightthickness=0).place(relx=0.999, rely=0.05, anchor='e')
     Button(app_power, text="Shutdown", command=shutdown, bg='#182427', fg='white', borderwidth=0, highlightthickness=0).pack()
     Button(app_power, text="Restart", command=restart, bg='#182427', fg='white', borderwidth=0, highlightthickness=0).pack()
-    Button(app_power, text="Sleep", command=sleep, bg='#182427', fg='white', borderwidth=0, highlightthickness=0).pack()
+    Button(app_power, text="Lock", command=lock, bg='#182427', fg='white', borderwidth=0, highlightthickness=0).pack()
     Button(app_power, text="Quit Xienu Bar", command=quitapp, bg='#182427', fg='white', borderwidth=0, highlightthickness=0).pack()
 
 
@@ -165,7 +173,6 @@ def apps():
     scrollbarx.config( command = list.xview )
     scrollbar.config( command = list.yview )
     
-    
 
 menu = tk.Tk()
 
@@ -195,6 +202,10 @@ def files():
     with pyautogui.hold('win'):
         pyautogui.press('e')
 
+def actions():
+    with pyautogui.hold('win'):
+        pyautogui.press('a')
+
 lbl = Button(menu, font = ('calibri', 10, 'bold'), bg='#182427', fg='white', command=lambda: calendar(), borderwidth=0, highlightthickness=0)
 lbl.place(relx = 0.982, rely = 0.45, anchor = 'e')
 times()
@@ -208,6 +219,7 @@ image = image.resize((33, 33), Image.ANTIALIAS)
 my_img = ImageTk.PhotoImage(image)
 Button(menu, text = '', image = my_img, command=windows, border=0, highlightthickness=0).place(relx = 0.005, rely = 0.48, anchor = 'w')
 
+Button(menu, text='≡', highlightthickness=0, border=0, bg='#182427', fg='white', command=actions, font=("", 18)).place(relx = 0.883, rely = 0.48, anchor = 'e')
 
 Button(menu, text = 'Power', bg='#182427', fg='white', border=0, command=power).place(relx = 0.035, rely = 0.48, anchor = 'w')
 Button(menu, text='Find',bg='#182427', fg='white', border=0, command=find).place(relx = 0.07, rely = 0.48, anchor = 'w')
@@ -220,15 +232,19 @@ Button(menu, text = 'Music', bg='#182427', fg='white', border=0, command=spotify
 def getlink(self):
     keyword = ''
     keyword = inputtxt.get(1.0, "end-1c")
-    webbrowser.open('http://www.google.com/search?q='+keyword)
-    inputtxt.delete(1.0, END)
+    if (keyword==''):
+        pass
+    else:
+        webbrowser.open('http://www.google.com/search?q='+keyword)
+        inputtxt.delete(1.0, END)
 
 global inputtxt
 w = Canvas(menu, width=120, height=0, highlightthickness=0.5)
-w.place(relx = 0.83, rely = 0.9, anchor = 's')
-Label(menu, text="Quick Search:", bg='#182427', fg='white', border=0).place(relx = 0.735, rely = 0.48, anchor = 'w')
+w.place(relx = 0.81, rely = 0.9, anchor = 's')
+Label(menu, text="Quick Search:", bg='#182427', fg='white', border=0).place(relx = 0.715, rely = 0.48, anchor = 'w')
 inputtxt = Text(menu, height=0, width=10, bg='#182427', fg='white', border=0, highlightthickness=0)
-inputtxt.place(relx = 0.86, rely = 0.45, anchor = 'e')
+inputtxt.place(relx = 0.84, rely = 0.45, anchor = 'e')
+
 menu.bind('<Return>', getlink)
 
 menu.mainloop()
